737a866836c15923af6d6ae01ad1b95dbf0128f4
Yugma Fernando <58908638+IT17167192@users.noreply.github.com>
2020-05-06 15:12:50 +0530

    Added comments to SensorAppMain.js

--- alarm-monitoring-client/src/main/SensorAppMain.js
+++ alarm-monitoring-client/src/main/SensorAppMain.js
@@ -5,6 +5,7 @@ import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 class SensorAppMain extends Component {
     constructor(props) {
         super(props);
+        //initial state
         this.state = {
             isLoading: true,
             sensors: []
@@ -12,25 +13,34 @@ class SensorAppMain extends Component {
     }
 
     getSensorState() {
+        //get all sensors
         fetch('http://localhost:8080/api/sensors')
             .then(response => {
                 return response.json();
             })
             .then(result => {
+                //set state and loading to false once we receive the all sensors
                 this.setState({isLoading: false, sensors: result});
             })
             .catch(error => {
+                //if there is an error
+                //display the error
                 console.log(error)
             });
     }
 
     componentDidMount() {
+        //after mount component into the ui
+        //get all sensors
+        //and assign to the state
         this.getSensorState();
         this.interval = setInterval(() => {
+            //periodically retrieve sensor state
             this.getSensorState();
         }, 40000);
     }
 
+    //rendering function call child Sensor.js
     render() {
         const {isLoading, sensors} = this.state;
         if (isLoading)
